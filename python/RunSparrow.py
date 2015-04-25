6eaa7ae6f6e5062ba97f566600b3573ac6a5c638
ThomasDq <thaomas.dubc@yahoo.fr>
2015-04-24 21:34:34 -0500

    [python] Script to run sparrow 6.0

--- python/RunSparrow.py
+++ python/RunSparrow.py
@@ -1,8 +1,8 @@
 import subprocess, os, shlex, time, sys
 
-os.chdir("/home/ubuntu/sparrow")
+os.chdir("/home/ubuntu/sparrow/python")
 subprocess.call("echo \"\" > Finish.txt", shell = True)
-commandFrontend = "java -cp /target/sparrow-1.0-SNAPSHOT.jar edu.berkeley.sparrow.examples.BFrontend -c /Conf/conf.Frontend1"
+commandFrontend = "java -cp ../target/sparrow-1.0-SNAPSHOT.jar edu.berkeley.sparrow.examples.BFrontend -c ../Conf/conf.Frontend1"
 
 startTime = time.time()
 sparrowFrontend = subprocess.Popen(shlex.split(commandFrontend))
@@ -11,7 +11,7 @@ while run and time.time() - startTime < 600:
     #wait for front end to finish
     time.sleep(5)
     print "Benchmark - check Finish.txt"
-    fd = open("Finish.txt", "r")
+    fd = open("../Finish.txt", "r")
     if fd.readline() == "Experience finished\n":
         run = False
         break
