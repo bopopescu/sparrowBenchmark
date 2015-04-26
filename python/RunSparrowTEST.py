7c20199237d1a4767d576b39270facbb47efd495
ThomasDq <thaomas.dubc@yahoo.fr>
2015-04-26 12:56:52 -0500

    python: add script and conf to test connection

---
+++ python/RunSparrowTEST.py
@@ -0,0 +1,27 @@
+import subprocess, os, shlex, time, sys
+
+os.chdir("/home/ubuntu/sparrow/python")
+subprocess.call("echo \"\" > Finish.txt", shell = True)
+commandFrontend = "java -cp ../target/sparrow-1.0-SNAPSHOT.jar edu.berkeley.sparrow.examples.BFrontend -c ../Conf/conf.Frontend"
+
+startTime = time.time()
+sparrowFrontend = subprocess.Popen(shlex.split(commandFrontend))
+run = True
+while run and time.time() - startTime < 600:
+    #wait for front end to finish
+    time.sleep(5)
+    print "Benchmark - check Finish.txt"
+    fd = open("Finish.txt", "r")
+    if fd.readline() == "Experience finished\n":
+        run = False
+        break
+    fd.close()
+
+sparrowFrontend.kill()
+
+print "Experience ended?" + str(not(run))
+
+if run:
+    sys.exit(1)
+else:
+    sys.exit(0)
